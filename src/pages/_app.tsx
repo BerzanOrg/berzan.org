ac1d35d9007f4af62187ea0630ed10adeb2d88aa
Berzan <berzan@tutanota.com>
2023-12-14 10:34:19 +0000

    use: `Plus Jakarta Sans` font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`         
+                html {
+                    font-family: ${plusJakartaSans.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    )
 }
