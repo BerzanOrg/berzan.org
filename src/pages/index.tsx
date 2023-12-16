c5f5f033d9f15221883eceb023592be922ce3eb8
Berzan <berzan@tutanota.com>
2023-12-16 10:23:49 +0000

    update: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,28 @@
-import FooterLink from "@/components/FooterLink";
+import { IconEmail, IconGithub, IconTwitter } from "@/components/Icons";
 import RouteLink from "@/components/RouteLink";
 import Image from "next/image";
 
-
 export default function Home() {
     return (
-        <div className="flex flex-col items-center min-h-screen gap-12 px-6 pt-16 pb-6 text-neutral-950 bg-gradient-to-tl from-yellow-300/90 to-yellow-200/90">
-            <header className="flex flex-col items-center gap-6">
-                <Image src='/myself.jpg' alt="Berzan" width={171} height={171} className="border rounded-full pointer-events-none select-none border-neutral-950" />
-                <div className="flex flex-col gap-1.5 text-center">
-                    <h1 className="text-6xl font-bold"> I'm Berzan. </h1>
-                    <p className="text-lg font-medium "> I love building tools & sharing information.</p>
-                </div>
+        <div className="flex flex-col gap-6 pb-24">
+            <header className="flex items-center justify-end w-full h-24 max-w-6xl gap-6 pr-6">
+                <IconEmail />
+                <IconTwitter />
+                <IconGithub />
             </header>
-            <main className="flex flex-col flex-1 w-full max-w-xl">
-                <nav className="flex flex-col">
+            <main className="flex flex-col items-center w-full gap-6">
+                <Image src='/myself.jpg' alt="Berzan" width={171} height={171} className="border rounded-full pointer-events-none select-none border-neutral-950" />
+                <div className="flex flex-col gap-1.5 text-center px-4">
+                    <h1 className="text-6xl font-bold"> I'm Berzan. </h1>
+                    <p className="font-bold text-l "> I love building tools & sharing information. </p>
+                </div>
+                <nav className="flex flex-col w-full max-w-xl px-6">
                     <ul className="flex flex-col gap-5">
                         <RouteLink href="/mina-blog" name="Mina Blog" />
                         <RouteLink href="/solana-blog" name="Solana Blog" />
                     </ul>
                 </nav>
             </main>
-            <footer className="flex justify-center">
-                <div className="flex flex-col gap-2.5">
-                    <FooterLink href="mailto:me@berzan.org" name="Email" content="me@berzan.org" />
-                    <FooterLink href="https://github.com/BerzanXYZ" name="GitHub" content="BerzanXYZ" />
-                    <FooterLink href="https://twitter.com/BerzanXYZ" name="Twitter" content="BerzanXYZ" />
-                </div>
-            </footer>
         </div>
     )
 }
