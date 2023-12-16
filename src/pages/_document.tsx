b12bb7e4edf067c8151d8f36cf98c8eb2629eab9
Berzan <berzan@tutanota.com>
2023-12-16 10:23:30 +0000

    update: body's class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='flex flex-col items-center text-neutral-950 bg-gradient-to-tl from-yellow-300/90 to-yellow-200/90'>
         <Main />
         <NextScript />
       </body>
