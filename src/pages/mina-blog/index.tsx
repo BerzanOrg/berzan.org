39e130b12f3b0a8136728646bc205d2f06578419
Berzan <berzan@tutanota.com>
2023-12-29 19:29:33 +0000

    remove: unused files

--- src/pages/mina-blog/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-    return (
-        <div className="flex flex-col items-center min-h-screen gap-12 px-6 pt-16 pb-6">
-            todo
-        </div>
-    )
-}
