df774f48e81bc4a7e9d5cdad58361fdbcb9c44ae
Berzan <berzan@tutanota.com>
2023-12-31 17:36:46 +0000

    create: default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<header
+	class="sticky top-0 flex h-18 items-center justify-between bg-yellow-50/80 px-4 backdrop-blur-md md:px-8"
+>
+	<a href="/" class="text-3xl font-bold text-cyan-500/80">Berzan</a>
+	<a
+		href="mailto:me@berzan.org"
+		class=" flex h-11 items-center justify-center rounded-full bg-cyan-500/80 px-7 text-lg font-bold text-white duration-200 hover:bg-cyan-500/60"
+		>Reach Me</a
+	>
+</header>
+
+<main class="flex flex-1 flex-col items-center justify-center gap-8">
+	<section class="w-full px-4 pt-2 md:px-8">
+		<h1 class="text-center text-6xl font-bold sm:text-7xl md:text-8xl">Error!</h1>
+		<p class="text-center text-base font-semibold sm:text-lg md:text-xl">
+			It looks like something unexpected is happened.
+		</p>
+		<p class="text-center text-base font-semibold sm:text-lg md:text-xl">
+			But you can still go to the <a href="/" class="hover:underline">homepage</a>.
+		</p>
+	</section>
+</main>
