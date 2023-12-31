11e4b9932fa2522c1f82f89bb9a44b9006d7e0ca
Berzan <berzan@tutanota.com>
2023-12-31 17:37:12 +0000

    add: new section

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -2,12 +2,23 @@
 	<title>Berzan's Homepage</title>
 </svelte:head>
 
-<main class="flex flex-1 flex-col items-center gap-8">
+<main class="flex flex-1 flex-col items-center gap-8 sm:gap-10 md:gap-12 lg:gap-18">
 	<section class="w-full px-4 pt-2 md:px-8">
 		<div
 			class="flex h-160 flex-col items-center justify-center rounded-2xl bg-[url('/palm-trees.webp')] bg-cover bg-center text-white md:h-180 md:rounded-3xl xl:h-200"
 		>
 			<h1 class="text-center text-6xl font-bold sm:text-7xl md:text-8xl">I'm Berzan.</h1>
 		</div>
+	</section>
+	<section class="w-full max-w-6xl px-4 pt-2 md:px-8">
+		<p class="text-center text-2xl font-semibold sm:text-3xl md:text-4xl lg:text-5xl">
+			I'm a developer,
+			<a href="https://minaprotocol.com/" target="_blank" class="hover:underline">Mina </a>
+			Navigator,
+			<br />
+			<a href="https://superteam.fun/" target="_blank" class="hover:underline">Superteam </a>
+			Member, just
+			<a href="https://twitter.com/BerzanOrg" target="_blank" class="hover:underline">myself</a>.
+		</p>
 	</section>
 </main>
