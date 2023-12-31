1c2fa35b24aa5443aec76787216b48cbeaf62f9d
Berzan <berzan@tutanota.com>
2023-12-31 19:42:42 +0000

    add: homepage content

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -2,7 +2,9 @@
 	<title>Berzan's Homepage</title>
 </svelte:head>
 
-<main class="flex flex-1 flex-col items-center gap-8 sm:gap-10 md:gap-12 lg:gap-18">
+<main
+	class="flex flex-1 flex-col items-center gap-32 pb-32 sm:gap-36 sm:pb-36 md:gap-48 md:pb-48 lg:gap-56 lg:pb-56"
+>
 	<section class="w-full px-4 pt-2 md:px-8">
 		<div
 			class="flex h-160 flex-col items-center justify-center rounded-2xl bg-[url('/palm-trees.webp')] bg-cover bg-center text-white md:h-180 md:rounded-3xl xl:h-200"
@@ -11,14 +13,55 @@
 		</div>
 	</section>
 	<section class="w-full max-w-6xl px-4 pt-2 md:px-8">
-		<p class="text-center text-2xl font-semibold sm:text-3xl md:text-4xl lg:text-5xl">
+		<p class="text-center text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">
 			I'm a developer,
 			<a href="https://minaprotocol.com/" target="_blank" class="hover:underline">Mina </a>
 			Navigator,
 			<br />
 			<a href="https://superteam.fun/" target="_blank" class="hover:underline">Superteam </a>
-			Member, just
+			Member, or just
 			<a href="https://twitter.com/BerzanOrg" target="_blank" class="hover:underline">myself</a>.
+		</p>
+	</section>
+	<section class="flex w-full max-w-6xl flex-col gap-4 px-4 pt-2 sm:gap-6 md:px-8 lg:gap-7">
+		<div class="flex flex-col gap-1 sm:gap-1.5 md:gap-2">
+			<h2 class="text-center text-5xl font-bold sm:text-6xl md:text-7xl">Berzan's Projects</h2>
+			<p class="text-center text-lg font-medium sm:text-xl md:text-2xl">
+				Below are the projects I'm building or I've built.
+			</p>
+		</div>
+		<ul class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-5">
+			{#snippet project(params)}
+				<li class="flex flex-col">
+					<a
+						href={params.link}
+						target="_blank"
+						class="flex flex-col gap-0.5 rounded-2xl bg-cyan-300/60 p-5 duration-200 hover:scale-95 hover:bg-cyan-300/40"
+					>
+						<h3 class="text-3xl font-bold sm:text-4xl md:text-3xl">
+							{params.name}
+						</h3>
+						<p class="text-lg font-medium sm:text-xl md:text-lg">{params.description}</p>
+					</a>
+				</li>
+			{/snippet}
+			{@render project({
+				name: 'Xane',
+				description: 'A zero knowledge decentralized exchange.',
+				link: 'https://xane.berzan.org/'
+			})}
+			{@render project({
+				name: 'Starknet Token Creator',
+				description: 'A tool to create tokens on Starknet.',
+				link: '/apps/starknet-token-creator'
+			})}
+		</ul>
+	</section>
+	<section class="w-full max-w-6xl px-4 pt-2 md:px-8">
+		<p class="text-center text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">
+			I like VMs, distributed systems,
+			<br />
+			cryptography, and data structures.
 		</p>
 	</section>
 </main>
